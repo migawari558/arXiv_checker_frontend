35d50536edde1f54d642e6e809ce300a49651f5d
migawari558 <tekitou@sample.com>
2025-05-14 13:06:44 +0900

    use client内でrevalidateしていたのを修正

--- src/app/user/(sidebar)/articles/page.tsx
+++ src/app/user/(sidebar)/articles/page.tsx
@@ -11,8 +11,6 @@ import {
 import { MyArticle } from "./component/MyArticle";
 import { Toaster } from "@/components/ui/toaster";
 
-export const revalidate = 30; // 30秒ごとに再検証する
-
 export default function MyArticles() {
   const dispatch = useAppDispatch();
   const { items, loading, error } = useAppSelector((state) => state.articles);
